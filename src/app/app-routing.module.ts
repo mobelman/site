061bfab2692ce0acfa2d51e113d624dba6521388
Andreas Kristensson <>
2018-01-12 11:29:56 +0100

    Replaces slash with hyphen in URLs.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,12 +11,12 @@ import {RequestInfoComponent} from './request-info/request-info.component';
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'churches', component: ChurchesComponent},
-  {path: 'church/detail/:id', component: ChurchDetailComponent},
+  {path: 'church-detail/:id', component: ChurchDetailComponent},
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'lego/production', component: LegoProductionComponent},
-  {path: 'private/products', component: PrivateProductsComponent},
-  {path: 'public/products', component: PublicProductsComponent},
-  {path: 'request/info', component: RequestInfoComponent}
+  {path: 'lego-production', component: LegoProductionComponent},
+  {path: 'private-products', component: PrivateProductsComponent},
+  {path: 'public-products', component: PublicProductsComponent},
+  {path: 'request-info', component: RequestInfoComponent}
 ];
 
 @NgModule({
