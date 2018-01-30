66b56498ceaad1761ef74679d64356d66ef74d05
Andreas Kristensson <>
2018-01-30 15:17:53 +0100

    Adds images to dashboard.

--- src/app/public-products/shared/public-products.ts
+++ src/app/public-products/shared/public-products.ts
@@ -2,8 +2,8 @@ import {PublicProduct} from './public-product.model';
 import {PRODUCTS} from '../../shared/products';
 
 export const PUBLIC_PRODUCTS: PublicProduct[] = [
-  PRODUCTS[6],
   PRODUCTS[7],
   PRODUCTS[8],
-  PRODUCTS[9]
+  PRODUCTS[9],
+  PRODUCTS[6]
 ];
